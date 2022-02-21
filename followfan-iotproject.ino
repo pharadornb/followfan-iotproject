01be13b48d5d4d278e1a89d6511ad5fa4bd49e97
Pharadorn Boonruam <61776315+pharadornb@users.noreply.github.com>
2022-02-21 12:56:13 +0700

    Success project

--- followfan-iotproject.ino
+++ followfan-iotproject.ino
@@ -1,1 +1,103 @@
+/* github project : https://github.com/pharadornb/followfan-iotproject */
+int relayPin = 2;
+int trigPin = 3;
+int echoPin = 4;
+long duration;
+int distance;
+#define motorPin0 5
+#define motorPin1 6
+#define motorPin2 7
+#define motorPin3 8
+int pirPin0 = 9;
+int pirPin1 = 10;
+int pirState0 = 0;
+int pirState1 = 0;
 
+void setup() {
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(motorPin0, OUTPUT);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(pirPin0, INPUT);
+  pinMode(pirPin1, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  pirState0 = digitalRead(pirPin0);
+  pirState1 = digitalRead(pirPin1);
+  
+  if (pirState0 == 1) {
+      if(distance <=30){
+        digitalWrite(relayPin, HIGH);
+        backwardMove();
+      }else if(distance > 30 && distance <=55){
+        breakMove();
+        digitalWrite(relayPin, LOW);
+        delay(6000);
+        digitalWrite(relayPin, HIGH);
+      } else {
+        digitalWrite(relayPin, HIGH);
+        forwardMove();
+      }
+ } else if (pirState1 == 1) {
+    leftMove()
+    delay(1500);
+ } else {
+    breakMove();
+    digitalWrite(relayPin, HIGH);
+ }
+}
+
+void breakMove() {
+  digitalWrite(motorPin0, HIGH);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, HIGH);
+}
+
+void forwardMove() {
+  digitalWrite(motorPin0, HIGH);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+}
+
+void backwardMove() {
+  digitalWrite(motorPin0, LOW);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+}
+
+void reverseMove() {
+  digitalWrite(motorPin0, LOW);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin2, HIGH);
+}
+
+void leftMove() {
+  digitalWrite(motorPin0, LOW);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+}
+
+void rightMove() {
+ digitalWrite(motorPin0, HIGH);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+}
--- sensor-test/sensor-test.ino
+++ sensor-test/sensor-test.ino
@@ -1,26 +1,26 @@
 
 /* pir sensor digital pin 9 + pin 10 */
-int digitalPin = 9;
-int val = 0;
-
-void setup() {
-  pinMode(digitalPin, INPUT);
-  Serial.begin(9600);
-}
-
-void loop() {
-  val = digitalRead(digitalPin);
-  
-  Serial.print("val = ");
-  Serial.println(val);
-  if (val == 0) {
-    Serial.print("Start");
-  }
-  else {
-    Serial.print("End");
-  }
-  delay(100);
-}
+//int digitalPin = 9;
+//int val = 0;
+//
+//void setup() {
+//  pinMode(digitalPin, INPUT);
+//  Serial.begin(9600);
+//}
+//
+//void loop() {
+//  val = digitalRead(digitalPin);
+//  
+//  Serial.print("val = ");
+//  Serial.println(val);
+//  if (val == 0) {
+//    Serial.print("Start");
+//  }
+//  else {
+//    Serial.print("End");
+//  }
+//  delay(100);
+//}
 
 /* pir sensor digital pin 9 */
 //const int pirPin = 9;
@@ -179,3 +179,54 @@ void loop() {
 //  digitalWrite(Relay1, HIGH); // ส่งให้ไฟดับ
 //  delay(2000); // ดีเลย์ 1000ms
 //}
+
+
+/* old code backup */
+
+//#define relayPin 2
+//#define trigPin 3
+//#define echoPin 4
+//#define motorPin0 5
+//#define motorPin1 6
+//#define motorPin2 7
+//#define motorPin3 8
+//int pirPin0 = 9;
+//#define pirPin1 10
+//
+//long duration;
+//int distance;
+//int pirState0 = 0;
+//int pirState1 = 0;
+//
+//void setup() {
+//  pinMode(relayPin, OUTPUT);
+//  digitalWrite(relayPin, HIGH);
+//  pinMode(echoPin, INPUT);
+//  pinMode(trigPin, OUTPUT);
+//  pinMode(motorPin0, OUTPUT);
+//  pinMode(motorPin1, OUTPUT);
+//  pinMode(motorPin2, OUTPUT);
+//  pinMode(motorPin3, OUTPUT);
+//  pinMode(pirPin0, INPUT);
+//  pinMode(pirPin1, INPUT);
+//  Serial.begin(9600);
+////  delay(15000);
+//}
+//
+//void loop() {
+//  pirState0 = digitalRead(pirPin0);
+//  pirState1 = digitalRead(pirPin1);
+//
+//  Serial.print(pirPin0);
+//
+////  if (pirState0 == 0) {
+////     Serial.print("Front start\n");
+////     delay(500);
+////  } else if (pirState0 == 0) {
+////     Serial.print("Back start\r");
+////     delay(500);
+////  } else {
+////     Serial.print("End\n");
+////  }
+//}
+//
